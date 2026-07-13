55b7441de9b2e0c86e0278c0e795d6ee94c61cd0
Tuan Le <tuanle9333@gmail.com>
2026-07-12 20:34:10 -0700

    Added Hand Values

---
+++ Assets/Scripts/Player/PlayerHandController.cs
@@ -0,0 +1,38 @@
+using Animancer;
+using UnityEngine;
+
+public class PlayerHandController : MonoBehaviour
+{
+	[Header("References")]
+	[SerializeField]
+	private AnimancerComponent _animancer;
+
+	[SerializeField]
+	private MyCharacterController _myCharacterController;
+
+	[Header("Animations")]
+	[SerializeField]
+	private AnimationClip _idleClip;
+
+	[SerializeField]
+	private AnimationClip _walkClip;
+
+	[Header("Settings")]
+	[SerializeField]
+	private float _crossFadeDuration = 0.25f;
+
+	private AnimationClip _currentClip;
+
+	private void Update()
+	{
+		bool isMoving = _myCharacterController.CurrentHorVelocity.sqrMagnitude > 0.01f;
+
+		AnimationClip targetClip = isMoving ? _walkClip : _idleClip;
+
+		if (targetClip != null && _currentClip != targetClip)
+		{
+			_currentClip = targetClip;
+			_animancer.Play(targetClip, _crossFadeDuration);
+		}
+	}
+}
